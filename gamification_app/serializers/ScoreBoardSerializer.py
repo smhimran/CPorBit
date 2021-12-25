0f7e08c7a078e32c5c498c5fcb944e5fb53f6f81
Shah Habibul Imran <smhimran@gmail.com>
2021-12-25 23:38:32 +0600

    [Back-end #63] Profile Score API

--- gamification_app/serializers/ScoreBoardSerializer.py
+++ gamification_app/serializers/ScoreBoardSerializer.py
@@ -1,4 +1,5 @@
 from gamification_app.models import ScoreBoard
+from problem_app.models import AcceptedSubmission
 from rest_framework import serializers
 
 
@@ -6,3 +7,14 @@ class StandingsScoreBoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = ScoreBoard
         fields = ('score', 'streak')
+
+
+class ProfileScoreSerializer(serializers.ModelSerializer):
+    solve_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ScoreBoard
+        fields = ('score', 'streak', 'solve_count')
+
+    def get_solve_count(self, obj):
+        return AcceptedSubmission.objects.filter(user=obj.user).count()
--- gamification_app/views.py
+++ gamification_app/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.filters import SearchFilter
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
+from gamification_app.models import ScoreBoard
 from gamification_app.paginations.StandingsPagination import \
     StandingsPagination
+from gamification_app.serializers.ScoreBoardSerializer import \
+    ProfileScoreSerializer
 from gamification_app.serializers.StandingsSerializer import \
     StandingsProfileSerializer
 
@@ -15,3 +21,20 @@ class StandingsViewset(viewsets.ReadOnlyModelViewSet):
     pagination_class = StandingsPagination
     filter_backends = (SearchFilter,)
     search_fields = ('username', 'first_name', 'last_name', 'profile__university', 'profile__cf_handle')
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def get_user_score(request):
+    username = request.GET.get('username')
+    scores = []
+    if username:
+        user = User.objects.get(username=username)
+        scores = ScoreBoard.objects.filter(user=user)
+        return Response(ProfileScoreSerializer(scores[0]).data, status=200)
+    elif request.user.is_authenticated:
+        scores = ScoreBoard.objects.filter(user=request.user)
+        return Response(ProfileScoreSerializer(scores[0]).data, status=200)
+    else:
+        return Response({'message': 'Bad  Request!'}, status=400)
+        
