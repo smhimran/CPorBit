755a45d085b3a147400675b030062bc8291e1873
Shah Habibul Imran <smhimran@gmail.com>
2021-12-26 17:25:57 +0600

    [Back-end #64] Profile Statistics API

--- gamification_app/urls.py
+++ gamification_app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from gamification_app.views import StandingsViewset, get_user_score
+from gamification_app.views import (StandingsViewset, get_user_score,
+                                    get_user_stats)
 
 router = DefaultRouter()
 
 router.register('', StandingsViewset)
 
 urlpatterns = [
-    path('profile/<username>/', get_user_score, name='profile-score'),
     path('', include(router.urls)),
+    path('profile/<username>/', get_user_score, name='profile-score'),
+    path('profile/<username>/stats/', get_user_stats, name='profile-stats'),
 ]
