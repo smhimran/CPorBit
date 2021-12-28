14dd5a56d954cf5b6cc4d22f082d30bd6db56d01
Shah Habibul Imran <smhimran@gmail.com>
2021-12-28 21:50:35 +0600

    [Back-end #67] Profile Hardest Solved Problem API

--- gamification_app/urls.py
+++ gamification_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from gamification_app.views import (StandingsViewset, get_user_score,
+from gamification_app.views import (StandingsViewset,
+                                    get_hardest_problem_solved, get_user_score,
                                     get_user_stats, get_user_strength,
                                     get_user_weakness)
 
@@ -15,4 +16,5 @@ urlpatterns = [
     path('profile/<username>/stats/', get_user_stats, name='profile-stats'),
     path('profile/<username>/strengths/', get_user_strength, name='profile-strengths'),
     path('profile/<username>/weaknesses/', get_user_weakness, name='profile-weakness'),
+    path('profile/<username>/hardest_solved_problem/', get_hardest_problem_solved, name='profile-hardest_solved_problem'),
 ]
