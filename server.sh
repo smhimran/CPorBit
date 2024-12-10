1ce72c711e01e612fb36c5f38e2c8c80ad2f1fde
Shah Habibul Imran <smhimran@gmail.com>
2024-12-10 11:03:08 +0600

    Fix script for different os

--- server.sh
+++ server.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --no-input
+# Check if Python command exists
+if command -v python3 &>/dev/null; then
+    PYTHON_CMD="python3"
+elif command -v python &>/dev/null; then
+    PYTHON_CMD="python"
+else
+    echo "Python is not installed. Please install Python to run this script."
+    exit 1
+fi
+
+# Run migration commands
+$PYTHON_CMD manage.py makemigrations
+$PYTHON_CMD manage.py migrate
+
+# Create the "staticfiles" directory
+if [ "$(uname)" == "Darwin" ] || [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    mkdir -p "public/staticfiles"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    mkdir "public/staticfiles"
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
+# Run the collectstatic command
+$PYTHON_CMD manage.py collectstatic --no-input
